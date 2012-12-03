be14832ac0bc7df46e9183b0a5ccff645a9791f0
unknown <nmeloccaro892@wit-xdvm-005.MILLARDPS.NET>
2012-12-03 11:57:33 -0600

    Added Folders

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
 <?php
+// Initialize the session
+session_start();
+
+// Load DB constants
+require('config/db.php');
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="icon" type="image/png" href="img/apple-icon.png" />
+
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+		
+		<title>The APPLE Blog</title>
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<?php include('layout/header.php')?>
+			</header>
+			<nav>
+				<?php include('layout/nav.php')?>
+			</nav>
+			<div id="message">
+				<?php include('layout/message.php')?>
+			</div>
+			<div id="content">
+				<?php include('layout/content.php')?>
+			</div>
+			<footer>
+				<?php include('layout/footer.php')?>
+			</footer>
+		</div>
+	</body>
+</html>
--- layout/nav.php
+++ layout/nav.php
@@ -1,1 +1,11 @@
-<?php
+<div class="navbar">
+	<div class="navbar-inner">
+		<div class="container">
+			<a href="./" class="brand"><img src="img/apple-icon.png" alt="" /></a>
+			<ul class="nav">
+				<li><a href="./?p=public/home">Home</a></li>
+				<li><a href="./?p=admin/form_addpost">Add</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
